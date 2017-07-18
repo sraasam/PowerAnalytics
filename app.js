121342efe74473b525228ad7b8c26ddf65a93bec
Sai C. Raasam <sraasam@v2softinc.com>
2017-07-18 09:08:54 -0400

    adding source code files

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express'); //including express   
+var app = new express(); // Creating instance   
+var port = process.env.PORT || 1337;// setting port for the application   
+//Following function is starts sockets and start listen from particular port. In following code I have given call back which contains err. So when port willbe start and listen function will be fire then this function will be execute.   
+app.listen(port, function(err) {  
+    if (typeof(err) == "undefined") {  
+        console.log('Your application is running on : ' + port + ' port');  
+    }  
+});
+app.use(express.static('public')); 
+app.use(express.static('src/views'));
+app.get('/', function(req, res) {  
+    res.send('<h1>Hello C# Corner.</h1>');  
+});  
+app.get('/articles', function(req, res) {  
+    res.send('<h1>Welcome to C# Corner Articles.</h1>');  
+});  
