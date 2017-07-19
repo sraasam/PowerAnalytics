24c0617f279cd2228e24fedcafae3a84a7a6f0ea
Sai C. Raasam <sraasam@v2softinc.com>
2017-07-19 10:11:50 -0400

    power bi integration

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express'); //including express   
-var app = new express(); // Creating instance   
+var app = new express(); // Creating instance  
+var path = require('path'),
+    fs = require('fs'); 
 var port = process.env.PORT || 1337;// setting port for the application   
 //Following function is starts sockets and start listen from particular port. In following code I have given call back which contains err. So when port willbe start and listen function will be fire then this function will be execute.   
 app.listen(port, function(err) {  
@@ -9,9 +11,14 @@ app.listen(port, function(err) {
 });
 app.use(express.static('public')); 
 app.use(express.static('src/views'));
+
+
 app.get('/', function(req, res) {  
     res.send('<h1>Hello C# Corner.</h1>');  
 });  
-app.get('/reports', function(req, res) {  
-    res.send('<h1>Welcome to PowerBI Reports</h1>');  
-});  
+
+app.get('/login', function(req, res) {  
+   // res.send('<h1>Welcome to PowerBI Reports</h1>');  
+   // res.sendFile('src/views/home.html');
+    res.sendFile('login.html', { root: './src/views/'});
+}); 
